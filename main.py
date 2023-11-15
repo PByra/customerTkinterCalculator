0028298425f172ff4bbeeaa5b69fbd72c7b49e0a
PByra <patrickbyra@gmail.com>
2023-11-15 00:19:40 -0500

    reorganized the repos and began work on the main appliaction menu

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import functions
+import tkinter as tk
+from tkinter import ttk
 
-def main(): 
-    functions.open_Window()
 
-if __name__ == "__main__": 
-    main()
+class main_application(tk.Frame):
+        def __init__(self, parent, *args, **kwargs):
+              tk.Frame.__init__(self, parent, *args, **kwargs)
+              self.parent = parent 
+              root.title("my application")
+              root.geometry("562x768")
+
+
+        #started button functionality 
+        def button_func(self):
+            print('a button was pressed')
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main_application(root)
+    root.mainloop()
