ef7fcf251b4298d15ea18fb00b9ee42c8f607897
PByra <patrickbyra@gmail.com>
2023-11-16 22:07:55 -0500

    sorted out the calculator set up, current issues are with updating the entry box, will continue this tomorrow

--- main.py
+++ main.py
@@ -4,29 +4,44 @@ import customtkinter as cttk
 class calculatorApp(cttk.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("300x500")
+        self.geometry("600x300")
         self.title("Calculator")
         
+        self.equation = cttk.StringVar()
+        self.expression = cttk.StringVar()
+        
         calculator_Widgets = [
-            ("button", "1", None),
-            ("button", "2", None),
-            ("button", "3", None),
-            ("button", "4", None),
-            ("button", "5", None),
-            ("button", "6", None),
-            ("button", "7", None),
-            ("button", "8", None),
-            ("button", "9", None),
-            ("button", "0", None),
-            ("button", "+", None),
-            ("button", "-", None),
-            ("button", "=", None),
-            ("button", "x", None),
-            ("button", "/", None),
+            ("button", "c", None, 0, 0),
+            ("button", "(", None, 0, 1),
+            ("button", ")", None, 0, 2),
+            ("button", "+", None, 0, 3),
+            ("button", "7", None, 1, 0),
+            ("button", "8", None, 1, 1),
+            ("button", "9", None, 1, 2),
+            ("button", "-", None, 1, 3),
+            ("button", "4", None, 2, 0),
+            ("button", "5", None, 2, 1),
+            ("button", "6", None, 2, 2),
+            ("button", "x", None, 2, 3),
+            ("button", "1", None, 3, 0),
+            ("button", "2", None, 3, 1),
+            ("button", "3", None, 3, 2),
+            ("button", "/", None, 3, 3),
+            ("button", "0", lambda: self.press(0), 4, 0),
+            ("button", "pi", None, 4, 1),
+            ("button", "e", None, 4, 2),
+            ("button", "=", None, 4, 3),
         ]
 
+        text_box = cttk.CTkEntry(self, textvariable = self.expression, height = 100, width = 580, font = ('Arial', 90))
+        text_box.grid(padx = 10, pady = 10)
+
         functionality_frame = frame_Gen(master = self, widgets = calculator_Widgets)
-        functionality_frame.grid(row = 4, column = 4, padx = 20, pady = 20, sticky = "nsew")
+        functionality_frame.grid(padx = 0, pady = 15, sticky = "s")
+    
+    def press(self, num): 
+        self.expression.set(self.equation.get() + str(num))
+            
 
 
 class frame_Gen(cttk.CTkFrame):
@@ -38,7 +53,7 @@ class frame_Gen(cttk.CTkFrame):
         #I will come back to this later, however at this time I would like to work on the 
         #functionality of the apps
         for row, widget_info in enumerate(widgets): 
-            widget_type, widget_text, widget_command = widget_info
+            widget_type, widget_text, widget_command, widget_row, widget_column = widget_info
 
             if widget_type == "button":
                 widget = cttk.CTkButton(self, text=widget_text, command = widget_command)
@@ -48,8 +63,7 @@ class frame_Gen(cttk.CTkFrame):
                 #place holder for other widgets 
                 continue
 
-            widget.pack(pady = 5, padx = 10)
-
+            widget.grid(row = widget_row, column = widget_column)
 
 
 class App(cttk.CTk):
@@ -63,10 +77,10 @@ class App(cttk.CTk):
         self.grid_columnconfigure(0, weight=1)
 
         main_Widgets = [
-            ("label", "Here are the Applications", None),
-            ("button", "Calculator App", self.button_Calculator),
-            ("button", "Sales Finder App", self.button_SalesFinder),
-            ("button", "Tangerine Finance App", self.button_TangerineFinance)
+            ("label", "Here are the Applications", None, None, None),
+            ("button", "Calculator App", self.button_Calculator, None, None),
+            ("button", "Sales Finder App", self.button_SalesFinder, None, None),
+            ("button", "Tangerine Finance App", self.button_TangerineFinance, None, None)
         ]
 
 
@@ -86,12 +100,10 @@ class App(cttk.CTk):
         print("Tangerine Finance App Opened")
 
     
-
 def main(): 
     app = App()
     app.mainloop()
 
+
 if __name__ == '__main__':
     main()
-
-
